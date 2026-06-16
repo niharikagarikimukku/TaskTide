c75cba4084f442536c8ffb952ee44cdddc9048ee
niharikagarikimukku <niharikagarikimukku070@gmail.com>
2026-06-16 11:09:25 +0530

    Add Firebase configuration file for login

---
+++ TaskTide-main/src/pages/login/firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDakKl_idZX1nDBa9PfQuA9SrDqr3BKhfo",
+  authDomain: "login-database-e9171.firebaseapp.com",
+  projectId: "login-database-e9171",
+  storageBucket: "login-database-e9171.appspot.com",
+  messagingSenderId: "1065399755718",
+  appId: "1:1065399755718:web:f2393632f70ef0048748c7"
+};
